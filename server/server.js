4bd3b6f248b0c5f04f8b16c55b6c0c32a3ca07f8
Scully87 <scully87.ts@googlemail.com>
2014-12-23 00:24:13 +0000

    installed and setup mongoDB

--- server/server.js
+++ server/server.js
@@ -7,6 +7,20 @@ var moment = require('moment');
 var mongoose = require('mongoose');
 var path = require('path');
 var request = require('request');
+
+var config = require('./config');
+
+var User = mongoose.model('User', new mongoose.Schema({
+  instagramId: { type: String, index: true },
+  email: { type: String, unique: true, lowercase: true },
+  password: { type: String, select: false },
+  username: String,
+  fullName: String,
+  picture: String,
+  accessToken: String
+}));
+ 
+mongoose.connect(config.db);
  
 var app = express();
  
